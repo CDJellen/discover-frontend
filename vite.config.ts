3b46e251a24b45efee543f21eb9ba1c199e6e1ad
CDJellen <64814231+CDJellen@users.noreply.github.com>
2023-02-01 12:16:52 -0800

    expose host and deprecate workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
+	resolve: { alias: { '@': '/src' } },
 	plugins: [sveltekit()],
+	server: {
+        host: true,
+        port: 5173
+    }
 };
 
 export default config;
