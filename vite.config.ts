8b3a83b383af08a6de2e7c481ee50c28aa333126
CDJellen <64814231+CDJellen@users.noreply.github.com>
2023-02-01 10:31:02 -0800

    deprecate api proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,6 @@ import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
 	plugins: [sveltekit()],
-	server:
-	{
-		proxy:
-		{
-			'/api': 'http://localhost:7071'
-		},
-	}
 };
 
 export default config;
