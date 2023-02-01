e9174445705f52c9b1eaacadb398cb78e6221795
CDJellen <64814231+CDJellen@users.noreply.github.com>
2023-02-01 13:17:33 -0800

    remove alias for `@`

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	resolve: { alias: { '@': '/src' } },
 	plugins: [sveltekit()],
 	server: {
         host: true,
